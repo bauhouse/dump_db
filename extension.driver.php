acee377b551b68422a7a3bf7c2baf08350f5ac1e
Stephen Bau <bauhouse@gmail.com>
2011-06-05 17:39:38 -0700

    Add the ability to dump and restore structural data
    Copy some code from the Export Ensemble extension and modify the code a little to be able to select the structural tables to both dump and restore without affecting entries data.

--- extension.driver.php
+++ extension.driver.php
@@ -107,6 +107,7 @@
 			
 			$span = new XMLElement('span', NULL, array('class' => 'frame'));
 			$span->appendChild(new XMLElement('button', __('Save Authors'), array_merge(array('name' => 'action[dump][authors]', 'type' => 'submit'), $disabled)));
+			$span->appendChild(new XMLElement('button', __('Save Structure'), array_merge(array('name' => 'action[dump][structure]', 'type' => 'submit'), $disabled)));
 			$span->appendChild(new XMLElement('button', __('Save Data'), array_merge(array('name' => 'action[dump][data]', 'type' => 'submit'), $disabled)));
 			$div->appendChild($span);
 			
@@ -118,6 +119,7 @@
 			
 			$span = new XMLElement('span', NULL, array('class' => 'frame'));
 			$span->appendChild(new XMLElement('button', __('Restore Authors'), array_merge(array('name' => 'action[restore][authors]', 'type' => 'submit'), $disabled)));
+			$span->appendChild(new XMLElement('button', __('Restore Structure'), array_merge(array('name' => 'action[restore][structure]', 'type' => 'submit'), $disabled)));
 			$span->appendChild(new XMLElement('button', __('Restore Data'), array_merge(array('name' => 'action[restore][data]', 'type' => 'submit'), $disabled)));
 			$div->appendChild($span);
 			
@@ -182,7 +184,13 @@
 			$restore = new MySQLRestore(Symphony::Database());
 			
 			$mode = NULL;
-			$mode = (isset($_POST['action']['restore']['authors']))? 'authors' : 'data';
+			if(isset($_POST['action']['restore']['authors'])) {
+				$mode = 'authors';
+			}
+			elseif(isset($_POST['action']['restore']['structure'])) {
+				$mode = 'structure';
+			}
+			else $mode = 'data';
 			if($mode == NULL) return;
 			
 			$filename = $this->generateFilename($mode);
@@ -211,13 +219,23 @@
 			$tables = array_map (create_function ('$x', 'return $x[0];'), $rows);
 			
 			$mode = NULL;
-			$mode = (isset($_POST['action']['dump']['authors']))? 'authors' : 'data';
+			if(isset($_POST['action']['dump']['authors'])) {
+				$mode = 'authors';
+			}
+			elseif(isset($_POST['action']['dump']['structure'])) {
+				$mode = 'structure';
+			}
+			else $mode = 'data';
+			
 			if($mode == NULL) return;
 			
 			$filename = $this->generateFilename($mode);
 			
+			## Find table prefix used for this install of Symphony
+			$tbl_prefix = Symphony::Configuration()->get('tbl_prefix', 'database');
+
 			foreach ($tables as $table){
-				$table = str_replace(Symphony::Configuration()->get('tbl_prefix', 'database'), 'tbl_', $table);
+				$table = str_replace($tbl_prefix, 'tbl_', $table);
 				
 				if($mode == 'authors') {
 					switch($table) {
@@ -231,6 +249,14 @@
 						default: // ignore everything but the authors
 							break;
 					}
+				}
+				if($mode == 'structure') {
+
+					## Create arrays of tables to dump
+					$db_tables = $this->__getDatabaseTables($tbl_prefix);
+					$structural_data_tables = $this->__getStructuralDataTables($db_tables);
+					$sql_data = $this->__dumpStructuralData($dump, $structural_data_tables);
+
 				}
 				elseif($mode == 'data') {
 					switch($table) {
@@ -284,4 +310,163 @@
 		private function generateFilename($mode) {
 			return sprintf($this->format, $mode);
 		}
+
+		private function __getDatabaseTables($tbl_prefix){
+
+			## Find all tables in the database
+			$Database = Symphony::Database();
+			$all_tables = $Database->fetch('show tables');
+
+			## Find length of prefix to test for table prefix
+			$prefix_length = strlen($tbl_prefix);
+
+			## Flatten multidimensional tables array
+			$db_tables = array();
+			foreach($all_tables as $table){
+				$value = array_values($table);
+				$value = $value[0];
+
+				## Limit array of tables to those using the table prefix
+				## and replace the table prefix with tbl
+				if(substr($value, 0, $prefix_length) === $tbl_prefix){
+					$db_tables[] = 'tbl_' . substr($value, $prefix_length);
+				}
+			}
+
+			return $db_tables;
+
+		}
+
+		private function __getStructureTables($structure_tables){
+
+			## Create array of tables to ignore for structure-only dump
+			$ignore_tables = array(
+				'tbl_entries_',
+				'tbl_fields_'
+			);
+
+			## Remove tables from list for structure-only dump
+			foreach($structure_tables as $index => $table){
+				foreach($ignore_tables as $starts){
+					if(substr($table, 0, strlen($starts)) === $starts ){
+						unset($structure_tables[$index]);
+					}
+				}
+			}
+
+			## Add fields tables back into list
+			$structure_tables[] = 'tbl_fields_%';
+			sort($structure_tables);
+
+			return $structure_tables;
+
+		}
+
+		private function __getDataTables($data_tables){
+
+			## Create array of tables to ignore for data-only dump
+			$ignore_tables = array(
+				'tbl_authors',
+				'tbl_cache',
+				'tbl_entries_',
+				'tbl_fields_',
+				'tbl_forgotpass',
+				'tbl_sessions'
+			);
+
+			## Remove tables from list for data-only dump
+			foreach($data_tables as $index => $table){
+				foreach($ignore_tables as $starts){
+					if(substr($table, 0, strlen($starts)) === $starts ){
+						unset($data_tables[$index]);
+					}
+				}
+			}
+
+			return $data_tables;
+
+		}
+
+		private function __getStructuralDataTables($tables){
+
+			## Create array of tables to ignore for structural data dump
+			$ignore_tables = array(
+				'tbl_authors',
+				'tbl_cache',
+				'tbl_entries',
+				'tbl_forgotpass',
+				'tbl_sessions'
+			);
+
+			## Remove tables from list for structural data dump
+			foreach($tables as $index => $table){
+				foreach($ignore_tables as $starts){
+					if(substr($table, 0, strlen($starts)) === $starts ){
+						unset($tables[$index]);
+					}
+				}
+			}
+
+			return $tables;
+
+		}
+
+		private function __dumpSchema($dump, $structure_tables, $tbl_prefix = NULL){
+
+			## Create variables for the dump files
+			$sql_schema = NULL;
+
+			## Grab the schema
+			foreach($structure_tables as $t) $sql_schema .= $dump->export($t, MySQLDump::STRUCTURE_ONLY);
+
+			if($tbl_prefix !== NULL) {
+				$sql_schema = str_replace('`' . $tbl_prefix, '`tbl_', $sql_schema);
+			}
+
+			$sql_schema = preg_replace('/AUTO_INCREMENT=\d+/i', NULL, $sql_schema);
+
+			return $sql_schema;
+
+		}
+
+		private function __dumpData($dump, $data_tables, $tbl_prefix = NULL){
+
+			## Create variables for the dump files
+			$sql_data = NULL;
+
+			## Field data and entry data schemas needs to be apart of the workspace sql dump
+			$sql_data  = $dump->export('tbl_fields_%', MySQLDump::ALL);
+			$sql_data .= $dump->export('tbl_entries_%', MySQLDump::ALL);
+
+			## Grab the data
+			foreach($data_tables as $t){
+				$sql_data .= $dump->export($t, MySQLDump::DATA_ONLY);
+			}
+
+			if($tbl_prefix !== NULL) {
+				$sql_schema = str_replace('`' . $tbl_prefix, '`tbl_', $sql_schema);
+			}
+
+			return $sql_data;
+
+		}
+
+		private function __dumpStructuralData($dump, $data_tables, $tbl_prefix = NULL){
+
+			## Create variables for the dump files
+			$sql_data = NULL;
+
+			## Grab the data
+			foreach($data_tables as $t){
+				$sql_data .= $dump->export($t, MySQLDump::ALL);
+			}
+
+			if($tbl_prefix !== NULL) {
+				$sql_schema = str_replace('`' . $tbl_prefix, '`tbl_', $sql_schema);
+			}
+
+			return $sql_data;
+
+		}
+
 	}
